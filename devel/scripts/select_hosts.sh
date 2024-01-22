b1549e81290bf430c1ba9fc3b5b8cb777d33460e
Shu Liu <liushu2011@outlook.com>
2024-01-22 15:43:41 -0500

    Delete devel/scripts directory

--- devel/scripts/select_hosts.sh
+++
@@ -1,7 +0,0 @@
-#！/bin/bash
-
-source /global/common/software/lsst/common/miniconda/kernels/setup.sh
-python /global/common/software/lsst/common/miniconda/start-kernel-cli.py desc-stack
-
-python select_hosts.py
-
