9217cd81fcb2cc26694decb5cde020f6cdd017af
Shu Liu <liushu2011@outlook.com>
2024-01-22 15:43:21 -0500

    Delete devel/config directory

--- devel/config/forcedConfig.py
+++
@@ -1,3 +0,0 @@
-config.measurement.plugins.names |= ["base_CircularApertureFlux"]
-# radii in pixels
-config.measurement.plugins["base_CircularApertureFlux"].radii = [1.0, 2.0, 3.0, 4.0, 5.0, 10.0, 20.0, 30.0, 40.0]
