80bbd989c638e9f0431f0ae9184277d3a884b8c1
Shu Liu <liushu2011@outlook.com>
2024-01-22 15:37:45 -0500

    Delete utils/devel directory

--- utils/devel/setup_desc_stack_weekly_latest.sh
+++
@@ -1,4 +0,0 @@
-#！/bin/bash
-
-source /global/common/software/lsst/common/miniconda/kernels/setup.sh
-python /global/common/software/lsst/common/miniconda/start-kernel-cli.py desc-stack-weekly-latest
